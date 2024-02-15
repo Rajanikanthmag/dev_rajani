d8c6cfd295f41ddd385c7b1e8248f60ccf5ea0b0
Rajanikanthmag <154207315+Rajanikanthmag@users.noreply.github.com>
2024-02-15 15:02:42 +0530

    Add files via upload

---
+++ Matta_fair_table_creation.sql
@@ -0,0 +1,23 @@
+
+USE CATALOG dih_stream;
+
+CREATE SCHEMA IF NOT EXISTS live_inventory;
+
+DROP TABLE IF EXISTS dih_stream.live_inventory.tbl_matta_fair;
+
+CREATE TABLE IF NOT EXISTS dih_stream.live_inventory.tbl_matta_fair (
+  flight_num String,
+  flight_date String,
+  origin String,
+  dest String,
+  dep_date String,
+  dep_time String,
+  ac_type String,
+  class String,
+  booked_count INTEGER,
+  available_count INTEGER,
+  capacity INTEGER
+);
+
+OPTIMIZE dih_stream.live_inventory.tbl_matta_fair;
+
