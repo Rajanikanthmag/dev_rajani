46de53d0f1a7c066f6d1635017963192b0aa998d
Rajanikanthmag <154207315+Rajanikanthmag@users.noreply.github.com>
2024-02-20 11:28:58 +0530

    Update Matta_fair_table_creation.sql

--- Matta_fair_table_creation.sql
+++ Matta_fair_table_creation.sql
@@ -1,22 +1,3 @@
-
-create procedure proc1
-AS
-BEGIN
-
-CREATE TABLE IF NOT EXISTS dih_stream.live_inventory.tbl_matta_fair (
-  flight_num String,
-  flight_date String,
-  origin String,
-  dest String,
-  dep_date String,
-  dep_time String,
-  ac_type String,
-  class String,
-  booked_count INTEGER,
-  available_count INTEGER,
-  capacity INTEGER
-)
-
-END
-
-
+WITH 
+  myvar AS (SELECT 'dih_stream.live_inventory.tbl_matta_fair' AS value)
+SELECT * FROM myvar;
